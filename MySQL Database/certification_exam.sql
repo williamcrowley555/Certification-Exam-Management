83bbdd5b718802794a2b7a896e638bf4825afa6d
William <buiquanghuy.10122000.10@gmail.com>
2021-09-20 15:42:00 +0700

    Update Database

--- MySQL Database/certification_exam.sql
+++ MySQL Database/certification_exam.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: certification_exam
+-- Host: localhost    Database: certification_exam
 -- ------------------------------------------------------
--- Server version	8.0.25
+-- Server version	8.0.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `english_level`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `english_level` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `english_level_name_unique` (`name`)
@@ -48,20 +48,20 @@ DROP TABLE IF EXISTS `exam`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `exam` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `listening_grade` int DEFAULT NULL,
-  `reading_grade` int DEFAULT NULL,
-  `speaking_grade` int DEFAULT NULL,
-  `writing_grade` int DEFAULT NULL,
-  `exam_room_id` bigint DEFAULT NULL,
-  `examine_id` bigint DEFAULT NULL,
-  `status` int NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `listening_grade` int(11) DEFAULT NULL,
+  `reading_grade` int(11) DEFAULT NULL,
+  `speaking_grade` int(11) DEFAULT NULL,
+  `writing_grade` int(11) DEFAULT NULL,
+  `exam_room_id` bigint(20) DEFAULT NULL,
+  `examine_id` bigint(20) DEFAULT NULL,
+  `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK2lntfak7rdl0c30rtdhwfalaj` (`exam_room_id`),
   KEY `FKjpsaveugt9xnhphp7x0xj96yr` (`examine_id`),
   CONSTRAINT `FK2lntfak7rdl0c30rtdhwfalaj` FOREIGN KEY (`exam_room_id`) REFERENCES `exam_room` (`id`),
   CONSTRAINT `FKjpsaveugt9xnhphp7x0xj96yr` FOREIGN KEY (`examine_id`) REFERENCES `examine` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `exam` (
 
 LOCK TABLES `exam` WRITE;
 /*!40000 ALTER TABLE `exam` DISABLE KEYS */;
-INSERT INTO `exam` VALUES (4,90,90,90,70,2,2,2),(5,60,70,80,100,2,1,2);
+INSERT INTO `exam` VALUES (4,90,90,90,70,2,2,2),(5,60,70,80,100,2,1,2),(6,0,0,0,0,10,3,1),(7,0,0,0,0,2,8,1),(8,0,0,0,0,2,14,1),(9,80,80,70,75,10,2,2);
 /*!40000 ALTER TABLE `exam` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,11 +82,11 @@ DROP TABLE IF EXISTS `exam_course`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `exam_course` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `month` int NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `month` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `year` int NOT NULL,
-  `english_level_id` bigint DEFAULT NULL,
+  `year` int(11) NOT NULL,
+  `english_level_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `exam_course_name_unique` (`name`),
   KEY `FKldcd13826uj9h70sxyoo24ipp` (`english_level_id`),
@@ -112,8 +112,8 @@ DROP TABLE IF EXISTS `exam_course_examine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `exam_course_examine` (
-  `exam_course_id` bigint NOT NULL,
-  `examine_id` bigint NOT NULL,
+  `exam_course_id` bigint(20) NOT NULL,
+  `examine_id` bigint(20) NOT NULL,
   PRIMARY KEY (`exam_course_id`,`examine_id`),
   KEY `FKdsed0twlip7syp1qbjvkb79r5` (`examine_id`),
   CONSTRAINT `FKdsed0twlip7syp1qbjvkb79r5` FOREIGN KEY (`examine_id`) REFERENCES `examine` (`id`),
@@ -127,7 +127,7 @@ CREATE TABLE `exam_course_examine` (
 
 LOCK TABLES `exam_course_examine` WRITE;
 /*!40000 ALTER TABLE `exam_course_examine` DISABLE KEYS */;
-INSERT INTO `exam_course_examine` VALUES (2,1),(2,2),(2,3),(2,4),(2,6),(1,7),(2,7),(1,8),(2,8),(1,9),(2,9),(1,10),(2,10),(1,11),(2,11),(1,12),(2,12),(1,13),(2,13),(1,14),(2,14),(1,15),(2,15),(1,16),(2,16),(1,17),(2,17),(1,18),(2,18),(1,19),(2,19),(1,20),(2,20),(1,21),(2,21),(1,22),(2,22),(1,23),(2,23),(1,24),(2,24),(1,25),(2,25),(1,26),(2,26),(1,27),(2,27),(1,28),(2,28),(1,29),(2,29),(1,30),(2,30),(1,31),(2,31),(1,32),(2,32),(1,33),(2,33),(2,34),(1,35),(2,35),(2,36),(1,37),(2,37),(2,38),(1,39);
+INSERT INTO `exam_course_examine` VALUES (2,1),(2,2),(2,3),(2,4),(2,6),(1,7),(2,7),(1,8),(2,8),(1,9),(2,9),(1,10),(2,10),(1,11),(2,11),(1,12),(2,12),(1,13),(2,13),(1,14),(2,14),(1,15),(2,15),(1,16),(2,16),(1,17),(2,17),(1,18),(2,18),(1,19),(2,19),(1,20),(2,20),(1,21),(2,21),(1,22),(2,22),(1,23),(2,23),(1,24),(2,24),(1,25),(2,25),(1,26),(2,26),(1,27),(2,27),(1,28),(2,28),(1,29),(2,29),(1,30),(2,30),(1,31),(2,31),(1,32),(2,32),(1,33),(2,33),(2,34),(1,35),(2,35),(2,36),(1,37),(2,37),(2,38),(1,39),(2,39),(2,40),(1,41),(2,41),(1,42),(2,42),(1,43),(1,44),(2,44),(1,45),(2,45),(2,46),(1,47),(2,47),(1,48),(2,48);
 /*!40000 ALTER TABLE `exam_course_examine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,13 +139,13 @@ DROP TABLE IF EXISTS `exam_room`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `exam_room` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `exam_date` datetime NOT NULL,
   `name` varchar(255) NOT NULL,
-  `quantity` int NOT NULL,
-  `exam_course_id` bigint DEFAULT NULL,
-  `examiner_id` bigint DEFAULT NULL,
-  `proctor_id` bigint DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `exam_course_id` bigint(20) DEFAULT NULL,
+  `examiner_id` bigint(20) DEFAULT NULL,
+  `proctor_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `exam_room_name_unique` (`name`),
   KEY `FKca524x1ealnr5ved1twn5tr77` (`exam_course_id`),
@@ -154,7 +154,7 @@ CREATE TABLE `exam_room` (
   CONSTRAINT `FK6i5hgfvu1p37p8ht7a6l643hg` FOREIGN KEY (`examiner_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FKah73e5vfgy3bm7eld48h4kd0b` FOREIGN KEY (`proctor_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FKca524x1ealnr5ved1twn5tr77` FOREIGN KEY (`exam_course_id`) REFERENCES `exam_course` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,7 +163,7 @@ CREATE TABLE `exam_room` (
 
 LOCK TABLES `exam_room` WRITE;
 /*!40000 ALTER TABLE `exam_room` DISABLE KEYS */;
-INSERT INTO `exam_room` VALUES (2,'2021-11-15 07:30:00','A2P01',30,1,3,2),(3,'2021-11-15 09:30:00','A2P02',0,1,3,2),(6,'2021-11-15 09:30:00','B1P01',0,2,2,3),(10,'2021-10-15 00:00:00','B1P02',3,2,4,2),(11,'2021-09-19 07:30:00','A2P03',0,1,NULL,4);
+INSERT INTO `exam_room` VALUES (2,'2021-11-15 07:30:00','A2P01',35,1,3,2),(3,'2021-11-15 09:30:00','A2P02',0,1,3,2),(6,'2021-11-15 09:30:00','B1P01',0,2,2,3),(10,'2021-10-15 15:00:00','B1P02',3,2,4,2),(11,'2021-09-19 07:30:00','A2P03',0,1,NULL,4),(12,'2021-09-20 10:00:00','A2P04',0,1,NULL,NULL),(13,'2021-10-22 15:00:00','B1P03',0,2,NULL,NULL),(15,'2021-11-18 08:00:00','A2P05',0,3,NULL,NULL);
 /*!40000 ALTER TABLE `exam_room` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,8 +175,8 @@ DROP TABLE IF EXISTS `exam_room_examine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `exam_room_examine` (
-  `exam_room_id` bigint NOT NULL,
-  `examine_id` bigint NOT NULL,
+  `exam_room_id` bigint(20) NOT NULL,
+  `examine_id` bigint(20) NOT NULL,
   PRIMARY KEY (`exam_room_id`,`examine_id`),
   KEY `FKnngglkhpopj11s2fw70pjhbaw` (`examine_id`),
   CONSTRAINT `FKah54ql6f4xfwxi06uy2ofu5jk` FOREIGN KEY (`exam_room_id`) REFERENCES `exam_room` (`id`),
@@ -190,7 +190,7 @@ CREATE TABLE `exam_room_examine` (
 
 LOCK TABLES `exam_room_examine` WRITE;
 /*!40000 ALTER TABLE `exam_room_examine` DISABLE KEYS */;
-INSERT INTO `exam_room_examine` VALUES (10,1),(10,2),(10,3),(2,7),(2,8),(2,9),(2,10),(2,11),(2,12),(2,13),(2,14),(2,15),(2,16),(2,17),(2,18),(2,19),(2,20),(2,21),(2,22),(2,23),(2,24),(2,25),(2,26),(2,27),(2,28),(2,29),(2,30),(2,31),(2,32),(2,33),(2,35),(2,37),(2,39);
+INSERT INTO `exam_room_examine` VALUES (10,1),(10,2),(10,3),(2,7),(2,8),(11,8),(2,9),(11,9),(2,10),(11,10),(2,12),(2,13),(2,14),(2,15),(2,16),(2,17),(2,18),(2,19),(11,19),(2,20),(2,21),(2,22),(2,23),(2,24),(2,25),(2,26),(2,27),(2,28),(2,29),(2,30),(2,31),(2,32),(2,33),(2,35),(2,37),(2,39),(2,41),(2,42),(2,43),(2,44),(2,45),(2,48);
 /*!40000 ALTER TABLE `exam_room_examine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -202,12 +202,12 @@ DROP TABLE IF EXISTS `examine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `examine` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `address` varchar(255) NOT NULL,
   `dob` date NOT NULL,
   `examine_id` varchar(30) DEFAULT NULL,
   `first_name` varchar(255) NOT NULL,
-  `gender` int NOT NULL,
+  `gender` int(11) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
@@ -223,7 +223,7 @@ CREATE TABLE `examine` (
 
 LOCK TABLES `examine` WRITE;
 /*!40000 ALTER TABLE `examine` DISABLE KEYS */;
-INSERT INTO `examine` VALUES (1,'Nha Trang','2001-06-14','B1003','Quốc Tuấn',1,'Trần','0776521474',1),(2,'Nha Trang','2000-09-11','B1001','Thị Thúy',2,'Lê','0906521766',1),(3,'Vũng Tàu','2000-09-06','B1008','Bảo Châu',2,'Lê','0904522158',1),(4,'TP.HCM','2000-09-06','B10010','Hoàng Minh',1,'Nguyễn','0908822128',1),(6,'TP.HCM','2000-09-30','B1009','Văn Linh',1,'Hoàng','0933633612',1),(7,'Hà Nội','2002-03-20','A2001','Vũ Minh',1,'Trần','0953622727',1),(8,'Hà Nội','1999-04-27','A2003','Mỹ Linh',2,'Nguyễn','0975375637',1),(9,'Hải Phòng','1997-05-14','A2002','Mậu Huy',1,'Trần','0963563276',1),(10,'Quãng Ninh','1999-08-22','A2005','Minh Tài',1,'Nguyễn','0973268678',1),(11,'Ninh Bình','1998-02-11','A2004','Thị Hoa',2,'Phạm','0912587572',1),(12,'Long An','2001-01-03','A2006','Ngọc Liên',2,'Trần','0915856756',1),(13,'Bình Dương','1999-01-02','A2007','Hùng Khang',1,'Lê','0915615776',1),(14,'Bạc Liêu','2002-04-22','A2008','Thị Diệu',2,'Trần','0965737357',1),(15,'Bến Tre','2000-06-23','A2009','Ngọc Diệu',2,'Phạm','0983567563',1),(16,'Bến Tre','1999-01-23','A20010','Anh Tài',1,'Nguyễn','0987586356',1),(17,'Bạc Liêu','1999-05-13',NULL,'Thị Ngọc',2,'Nguyễn','0935873275',1),(18,'Đà Nẵng','2002-03-12',NULL,'Minh Đạt',1,'Trần','0964747123',1),(19,'Đà Nẵng','2000-03-12',NULL,'Văn Khải',1,'Trần','0976575575',1),(20,'Ninh Thuận','2001-05-12',NULL,'Ngọc Như',2,'Trần','0946347277',1),(21,'Bình Thuận','2002-07-12',NULL,'Minh Hưng',1,'Nguyễn','0964646626',1),(22,'Tiền Giang','2003-03-12',NULL,'Thị Lệ',2,'Nguyễn','0968636787',1),(23,'Gò Công','2002-03-12',NULL,'Thái Mỹ',2,'Phạm','0946636439',1),(24,'Đồng Tháp','2003-09-12',NULL,'Thị Như',2,'Lại','0936474737',1),(25,'Gò Công','2002-12-12',NULL,'Nhật Long',1,'Tăng','0937747575',1),(26,'Long An','2002-03-12',NULL,'Ngọc Trân',2,'Nguyễn','0967684646',1),(27,'Cà Mau','2002-03-22',NULL,'Thị Ngọc',2,'Phạm','0968666372',1),(28,'Cà Mau','2002-04-11',NULL,'Văn Hùng',1,'Võ','0915325758',1),(29,'Bạc Liêu','2002-03-12',NULL,'Văn Hùng',1,'Nguyễn','0932623686',1),(30,'Bạc Liêu','2002-03-12',NULL,'Minh Duy',1,'Trần','0915235575',1),(31,'Sóc Trăng','2002-04-12',NULL,'Ngọc Diệp',2,'Lê','0934634637',1),(32,'Ninh Thuận','2001-03-12',NULL,'Thị Như',2,'Lê','0946347437',1),(33,'Hải Phòng','2002-03-12',NULL,'Văn Kiệt',1,'Hà','0946363678',1),(34,'Hải Phòng','2002-12-22',NULL,'Mạnh Nam',1,'Nguyễn','0924373475',1),(35,'Quãng Ngãi','2002-11-10',NULL,'Mỹ Lệ',2,'Trần','0934717183',1),(36,'Quãng Ngãi','2002-10-12',NULL,'Minh Hùng',1,'Lê','0917437438',1),(37,'Hà Giang','2002-03-17',NULL,'Ngọc Như',2,'Lê','0915873562',1),(38,'Kiên Giang','2002-03-14',NULL,'Văn Huy',1,'Lê','0912581756',1),(39,'Tây Ninh','2001-07-22',NULL,'Ngọc Anh',2,'Vũ','0923871623',1),(40,'Tây Ninh','2001-09-11',NULL,'Ngọc Ánh',2,'Vũ','0923728673',1),(41,'Đồng Nai','2003-03-12',NULL,'Minh Anh',1,'Trần','0923572836',1),(42,'Đồng Nai','2004-08-12',NULL,'Ngọc Nga',2,'Hà','0912517865',1),(43,'Bình Thuận','2002-05-12',NULL,'Minh Long',1,'Lê','0957238572',1),(44,'Bình Thuận','2004-03-12',NULL,'Minh Ngọc',2,'Nguyễn','0935782367',1),(45,'Khánh Hòa','2001-09-12',NULL,'Minh Anh',1,'Phạm','0935823766',2),(46,'Hà Nội','2003-11-12',NULL,'Minh Hùng',1,'Phạm','0923628767',2),(47,'TPHCM','1999-08-09',NULL,'Hải Hưng',1,'Phạm','0932687283',2),(48,'TPHCM','1999-12-11',NULL,'Văn Hải',1,'Lại','0923273872',2);
+INSERT INTO `examine` VALUES (1,'Nha Trang','2001-06-14','B1003','Quốc Tuấn',1,'Trần','0776521474',1),(2,'Nha Trang','2000-09-11','B1001','Thị Thúy',2,'Lê','0906521766',1),(3,'Vũng Tàu','2000-09-06','B1008','Bảo Châu',2,'Lê','0904522158',1),(4,'TP.HCM','2000-09-06','B10010','Hoàng Minh',1,'Nguyễn','0908822128',1),(6,'TP.HCM','2000-09-30','B1009','Văn Linh',1,'Hoàng','0933633612',1),(7,'Hà Nội','2002-03-20','A2001','Vũ Minh',1,'Trần','0953622727',1),(8,'Hà Nội','1999-04-27','A2011','Mỹ Linh',2,'Nguyễn','0975375637',1),(9,'Hải Phòng','1997-05-14','A2010','Mậu Huy',1,'Trần','0963563276',1),(10,'Quãng Ninh','1999-08-22','A2012','Minh Tài',1,'Nguyễn','0973268678',1),(11,'Ninh Bình','1998-02-11','A2004','Thị Hoa',2,'Phạm','0912587572',1),(12,'Long An','2001-01-03','A2006','Ngọc Liên',2,'Trần','0915856756',1),(13,'Bình Dương','1999-01-02','A2007','Hùng Khang',1,'Lê','0915615776',1),(14,'Bạc Liêu','2002-04-22','A2008','Thị Diệu',2,'Trần','0965737357',1),(15,'Bến Tre','2000-06-23','A2009','Ngọc Diệu',2,'Phạm','0983567563',1),(16,'Bến Tre','1999-01-23','A20010','Anh Tài',1,'Nguyễn','0987586356',1),(17,'Bạc Liêu','1999-05-13',NULL,'Thị Ngọc',2,'Nguyễn','0935873275',1),(18,'Đà Nẵng','2002-03-12',NULL,'Minh Đạt',1,'Trần','0964747123',1),(19,'Đà Nẵng','2000-03-12','A2013','Văn Khải',1,'Trần','0976575575',1),(20,'Ninh Thuận','2001-05-12',NULL,'Ngọc Như',2,'Trần','0946347277',1),(21,'Bình Thuận','2002-07-12',NULL,'Minh Hưng',1,'Nguyễn','0964646626',1),(22,'Tiền Giang','2003-03-12',NULL,'Thị Lệ',2,'Nguyễn','0968636787',1),(23,'Gò Công','2002-03-12',NULL,'Thái Mỹ',2,'Phạm','0946636439',1),(24,'Đồng Tháp','2003-09-12',NULL,'Thị Như',2,'Lại','0936474737',1),(25,'Gò Công','2002-12-12',NULL,'Nhật Long',1,'Tăng','0937747575',1),(26,'Long An','2002-03-12',NULL,'Ngọc Trân',2,'Nguyễn','0967684646',1),(27,'Cà Mau','2002-03-22',NULL,'Thị Ngọc',2,'Phạm','0968666372',1),(28,'Cà Mau','2002-04-11',NULL,'Văn Hùng',1,'Võ','0915325758',1),(29,'Bạc Liêu','2002-03-12',NULL,'Văn Hùng',1,'Nguyễn','0932623686',1),(30,'Bạc Liêu','2002-03-12',NULL,'Minh Duy',1,'Trần','0915235575',1),(31,'Sóc Trăng','2002-04-12',NULL,'Ngọc Diệp',2,'Lê','0934634637',1),(32,'Ninh Thuận','2001-03-12',NULL,'Thị Như',2,'Lê','0946347437',1),(33,'Hải Phòng','2002-03-12',NULL,'Văn Kiệt',1,'Hà','0946363678',1),(34,'Hải Phòng','2002-12-22',NULL,'Mạnh Nam',1,'Nguyễn','0924373475',1),(35,'Quãng Ngãi','2002-11-10',NULL,'Mỹ Lệ',2,'Trần','0934717183',1),(36,'Quãng Ngãi','2002-10-12',NULL,'Minh Hùng',1,'Lê','0917437438',1),(37,'Hà Giang','2002-03-17',NULL,'Ngọc Như',2,'Lê','0915873562',1),(38,'Kiên Giang','2002-03-14',NULL,'Văn Huy',1,'Lê','0912581756',1),(39,'Tây Ninh','2001-07-22',NULL,'Ngọc Anh',2,'Vũ','0923871623',1),(40,'Tây Ninh','2001-09-11',NULL,'Ngọc Ánh',2,'Vũ','0923728673',1),(41,'Đồng Nai','2003-03-12','A2014','Minh Anh',1,'Trần','0923572836',1),(42,'Đồng Nai','2004-08-12','A2016','Ngọc Nga',2,'Hà','0912517865',1),(43,'Bình Thuận','2002-05-12','A2015','Minh Long',1,'Lê','0957238572',1),(44,'Bình Thuận','2004-03-12','A2017','Minh Ngọc',2,'Nguyễn','0935782367',1),(45,'Khánh Hòa','2001-09-12','A2018','Minh Anh',1,'Phạm','0935823766',1),(46,'Hà Nội','2003-11-12',NULL,'Minh Hùng',1,'Phạm','0923628767',2),(47,'TPHCM','1999-08-09',NULL,'Hải Hưng',1,'Phạm','0932687283',2),(48,'TPHCM','1999-12-11','A2019','Văn Hải',1,'Lại','0923273872',1);
 /*!40000 ALTER TABLE `examine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -235,7 +235,7 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `normalized_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
@@ -262,13 +262,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `address` varchar(255) NOT NULL,
   `dob` date NOT NULL,
   `email` varchar(255) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   `first_name` varchar(255) NOT NULL,
-  `gender` int NOT NULL,
+  `gender` int(11) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
@@ -296,8 +296,8 @@ DROP TABLE IF EXISTS `user_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_role` (
-  `user_id` bigint NOT NULL,
-  `role_id` bigint NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
   KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
   CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
@@ -324,4 +324,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-19 17:36:22
+-- Dump completed on 2021-09-20 15:41:00
